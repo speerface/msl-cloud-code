9301924cc6d73e892edc80a4f4e7ba7ada1bb3a1
Aaron Speer <aaron@westwerk.com>
2016-06-24 21:27:08 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,118 @@
+var express    = require( 'express' );
+var Parse      = require( 'parse/node' );
+var cloud      = require( './lib/cloud' );
+var CloudCode  = require( './lib/hooks' );
+var bodyParser = require( 'body-parser' );
+var app        = express();
+
+app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use( bodyParser.json() );
+
+app.get('/', function (req, res) {
+
+    CloudCode.trigger( 'beforeSave', 'Action', { object: 'hey there' } ).then( function( response ){
+        res.send( JSON.stringify( response ) );
+    });
+
+});
+
+app.get('/testPost', function(req, res) {
+    var Log = Parse.Object.extend( 'Log' );
+    var deleteLog = new Log();
+
+    deleteLog.set( 'error', 'not really an error, just testing' );
+    deleteLog.set( 'guid', 'testguid-12345' );
+    deleteLog.save().then( function( response ) {
+        console.log( response );
+       res.send( response.id );
+    },
+    function( error ) {
+        res.send( JSON.stringify( error ) );
+    });
+});
+
+app.post( '/beforeSave', function(req, res) {
+    var type  = req.body.type,
+        data  = req.body.data,
+        isNew = req.body.new,
+        user  = req.body.user;
+
+    parseObject( isNew, type, data ).then( function( obj ) {
+        CloudCode.trigger( 'beforeSave', type, { object: obj, user: user } ).then( function( response ){
+            res.send( JSON.stringify( response ) );
+        });
+    });
+});
+
+app.post( '/afterSave', function(req, res) {
+    var type  = req.body.type,
+        data  = req.body.data,
+        isNew = false,
+        user  = req.body.user;
+
+    parseObject( isNew, type, data ).then( function( obj ) {
+        CloudCode.trigger( 'afterSave', type, { object: obj, user: user } ).then( function( response ){
+            res.send( JSON.stringify( response ) );
+        });
+    });
+});
+
+app.post( '/beforeDelete', function(req, res) {
+    var type  = req.body.type,
+        data  = req.body.data,
+        isNew = false,
+        user  = req.body.user;
+
+    parseObject( isNew, type, data ).then( function( obj ) {
+        CloudCode.trigger( 'beforeDelete', type, { object: obj, user: user } ).then( function( response ){
+            console.log( response );
+            res.send( JSON.stringify( response ) );
+        });
+    });
+});
+
+app.post( '/afterDelete', function(req, res) {
+
+    var type  = req.body.type,
+        data  = req.body.data,
+        isNew = false,
+        user  = req.body.user;
+
+    parseObject( isNew, type, data ).then( function( obj ) {
+        CloudCode.trigger( 'afterDelete', type, { object: obj, user: user } ).then( function( response ){
+            res.send( JSON.stringify( response ) );
+        });
+    });
+});
+
+app.listen(3000, function () {
+    Parse.initialize( "uDurhUNh3PWciPFDcUH2DgsKGkbsNCSYg9sd8J3K", "zVw4LTe6k2QmD4n2L2gPdMradqoobe5QXTwsirHE" );
+    Parse.serverURL = 'http://192.168.0.13/1';
+});
+
+function parseObject( isNew, type, data ) {
+
+    return new Promise( function( resolve, reject ) {
+        var object = Parse.Object.extend( type );
+        if ( isNew ) {
+            var self = new object();
+            for ( var key in data ) {
+
+                self.set( key, data[key] );
+
+            }
+            resolve( self );
+        } else {
+            var query = new Parse.Query( object );
+            query.get( data.objectId ).then(
+                function( object ) {
+                    resolve( object );
+                },
+                function( error ) {
+                }
+            );
+        }
+    });
+
+
+}
