3842883420279cb964401aa099a0932fafd9fc60
speerface <aaron@tri.be>
2017-03-30 20:05:58 -0500

    remove other logs

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post( '/afterSave', function(req, res) {
 
     parseObject( isNew, type, data, app_id ).then( function( obj ) {
 
-        console.log( obj.length );
+        console.log( Object.keys(obj).length === 0 && obj.constructor === Object );
 
         if ( ! obj.length ) {
             res.send( JSON.stringify( { 'error': 'Could not connect to api server.' } ) );
